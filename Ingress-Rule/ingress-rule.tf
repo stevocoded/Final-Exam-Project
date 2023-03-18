f888831e12707b48ccd3f00b90f04d9ebb3498fa
Saheed Anipupo <saheedanipupo@gmail.com>
2023-03-18 13:52:26 +0100

    Create ingress-rule.tf

---
+++ Ingress-Rule/ingress-rule.tf
@@ -0,0 +1,60 @@
+resource "kubernetes_ingress_v1" "voting-ingress" {
+  metadata {
+    name      = "azure-vote-front"
+    labels = {
+      name = "azure-vote-front"
+    }
+    annotations = {
+      "kubernetes.io/ingress.class" : "nginx"
+    }
+  }
+
+  spec {
+    rule {
+      host = "voting.stevocoded.me"
+      http {
+        path {
+          backend {
+            service{
+              name = "azure-vote-front"
+              port {
+                number = 80
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+}
+
+resource "kubernetes_ingress_v1" "micro-ingress" {
+  metadata {
+    name      = "sock-shop"
+    namespace = "sock-shop"
+    labels = {
+      name = "front-end"
+    }
+    annotations = {
+      "kubernetes.io/ingress.class" : "nginx"
+    }
+  }
+
+  spec {
+    rule {
+      host = "sock-shop.stevocoded.me"
+      http {
+        path {
+          backend {
+            service{
+              name = "front-end"
+              port {
+                number = 80
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+}
