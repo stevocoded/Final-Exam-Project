a1b52e64c63be7b3d68728f431079d649d86fd68
Saheed Anipupo <saheedanipupo@gmail.com>
2023-03-18 13:59:48 +0100

    Create prom-provider.tf

---
+++ Prometheus-Helm/prom-provider.tf
@@ -0,0 +1,57 @@
+terraform {
+  required_providers {
+    helm = {
+      source  = "hashicorp/helm"
+      version = ">= 2.0.0"
+    }
+
+    kubernetes = {
+        version = ">= 2.0.0"
+        source = "hashicorp/kubernetes"
+    }
+
+    kubectl = {
+      source = "gavinbunney/kubectl"
+      version = "1.14.0"
+    }
+  }
+}
+
+
+data "aws_eks_cluster" "stevo" {
+  name = "stevo"
+}
+data "aws_eks_cluster_auth" "stevo_auth" {
+  name = "stevo_auth"
+}
+
+
+provider "aws" {
+  region     = var.region
+
+}
+
+provider "helm" {
+    kubernetes {
+       #host                   = data.aws_eks_cluster.stevo.endpoint
+      # cluster_ca_certificate = base64decode(data.aws_eks_cluster.stevo.certificate_authority[0].data)
+       #token                  = data.aws_eks_cluster_auth.stevo_auth.token
+       config_path = "~/.kube/config"
+    }
+}
+
+provider "kubernetes" {
+  #host                   = data.aws_eks_cluster.stevo.endpoint
+ # cluster_ca_certificate = base64decode(data.aws_eks_cluster.stevo.certificate_authority[0].data)
+  #token                  = data.aws_eks_cluster_auth.stevo_auth.token
+ #  version          = "2.16.1"
+  config_path = "~/.kube/config"
+}
+
+provider "kubectl" {
+   load_config_file = false
+   host                   = data.aws_eks_cluster.stevo.endpoint
+   cluster_ca_certificate = base64decode(data.aws_eks_cluster.stevo.certificate_authority[0].data)
+   token                  = data.aws_eks_cluster_auth.stevo_auth.token
+    config_path = "~/.kube/config"
+}
